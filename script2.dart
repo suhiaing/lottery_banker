8a8ee9c157c20646eaafe80b90f85a3024258576
Su Hlaing Thin <115348141+suhiaing@users.noreply.github.com>
2023-12-25 23:03:46 +0630

    check_name fun done

--- script2.dart
+++ script2.dart
@@ -7,10 +7,19 @@ void main() {
   for (int i = 0; i < 5; i++) {
     add_new_gambler_to_db(just_test_delete[i]);
   }
+  change_name();
+  print(gambler_list[0].name);
+  print(gambler_list[1].name);
+  print(gambler_list[2].name);
+  print(gambler_list[3].name);
+  print(gambler_list[4].name);
 }
 
 class Gambler {
-  String name = "New Unknown Gambler Name";
+  String name = "Unknown";
+  Map<String, int> numbers_and_prices = {
+    "345": 34,
+  };
 }
 
 void add_new_gambler_to_db(String name_of_gambler_for_delete) {
@@ -37,13 +46,19 @@ bool duplicate_name(String name_for_testing) {
   return true;
 }
 
-void change_the_name() {
-  String name_to_search = "";
+void change_name() {
+  String name_to_search = "name";
   String new_name = "name_changed";
   gambler_list_length = gambler_list.length;
-  for (int i = 0; i < gambler_list_length; i++) {
-    if (name_to_search == gambler_list[i].name) {
-      gambler_list[i].name = new_name;
+  for (int i = 0; i <= gambler_list_length; i++) {
+    if (gambler_list_length != i) {
+      if (name_to_search == gambler_list[i].name) {
+        gambler_list[i].name = new_name;
+        print("$name_to_search has changed to $new_name");
+        return;
+      }
+    } else {
+      print("name doesn't found");
     }
   }
 }

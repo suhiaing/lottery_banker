809858fcda5b392b6fb339b6a09de7d75aebfba2
Su Hlaing Thin <115348141+suhiaing@users.noreply.github.com>
2023-12-25 23:41:44 +0630

    Update script2.dart

--- script2.dart
+++ script2.dart
@@ -1,14 +1,15 @@
 List<Gambler> gambler_list = [];
-bool pass = false;
 int gambler_list_length = gambler_list.length;
 
-List<String> just_test_delete = ["name1", "name2", "name3", "name4", "name5"];
+List<String> just_test_delete = ["name1", "name1", "name3", "name4", "name5"];
+
 void main() {
   for (int i = 0; i < 5; i++) {
     add_new_gambler_to_db(just_test_delete[i]);
   }
   change_name();
   print(gambler_list[0].name);
+  // print(gambler_list[0].numbers_and_prices);
   print(gambler_list[1].name);
   print(gambler_list[2].name);
   print(gambler_list[3].name);
@@ -17,9 +18,9 @@ void main() {
 
 class Gambler {
   String name = "Unknown";
-  Map<String, int> numbers_and_prices = {
-    "345": 34,
-  };
+  // Map<String, int> numbers_and_prices = {
+  //   "345": 34,
+  // };
 }
 
 void add_new_gambler_to_db(String name_of_gambler_for_delete) {
@@ -29,15 +30,18 @@ void add_new_gambler_to_db(String name_of_gambler_for_delete) {
   if (duplicate_name(new_gamler_in_angtd_fun.name) == true) {
     gambler_list.add(new_gamler_in_angtd_fun);
   }
+  // print("adding Numbers for ${new_gamler_in_angtd_fun.name}:");
+  // String no = "345";
+  // int pri = 30;
+  // Map<String, int> new_number_in_angtd_fun = {no: pri};
 }
 
 bool duplicate_name(String name_for_testing) {
-  pass = false;
   gambler_list_length = gambler_list.length;
   if (gambler_list_length > 0) {
     for (int i = 0; i < gambler_list_length; i++) {
       if (name_for_testing == gambler_list[i].name) {
-        return pass = false;
+        return false;
       }
     }
   } else {
@@ -47,7 +51,7 @@ bool duplicate_name(String name_for_testing) {
 }
 
 void change_name() {
-  String name_to_search = "name";
+  String name_to_search = "name1";
   String new_name = "name_changed";
   gambler_list_length = gambler_list.length;
   for (int i = 0; i <= gambler_list_length; i++) {

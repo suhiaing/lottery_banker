120181231df2e38c1925c0e62e770591af457558
Su Hlaing Thin <115348141+suhiaing@users.noreply.github.com>
2023-12-26 16:24:49 +0630

    change name fun update

--- script2.dart
+++ script2.dart
@@ -1,13 +1,13 @@
 List<Gambler> gambler_list = [];
 int gambler_list_length = gambler_list.length;
-
-List<String> just_test_delete = ["name1", "name1", "name3", "name4", "name5"];
+int pass = 0;
+List<String> just_test_delete = ["name1", "name2", "name3", "name4", "name5"];
 
 void main() {
   for (int i = 0; i < 5; i++) {
     add_new_gambler_to_db(just_test_delete[i]);
   }
-  change_name();
+  print(gambler_list);
   print(gambler_list[0].name);
   // print(gambler_list[0].numbers_and_prices);
   print(gambler_list[1].name);
@@ -27,42 +27,36 @@ void add_new_gambler_to_db(String name_of_gambler_for_delete) {
   Gambler new_gamler_in_angtd_fun = Gambler();
   //print("enter the name of gambler");
   new_gamler_in_angtd_fun.name = name_of_gambler_for_delete;
-  if (duplicate_name(new_gamler_in_angtd_fun.name) == true) {
+  pass = duplicate_name(new_gamler_in_angtd_fun.name);
+  if (pass == -1) {
     gambler_list.add(new_gamler_in_angtd_fun);
   }
-  // print("adding Numbers for ${new_gamler_in_angtd_fun.name}:");
-  // String no = "345";
-  // int pri = 30;
-  // Map<String, int> new_number_in_angtd_fun = {no: pri};
+  print("adding Numbers for ${new_gamler_in_angtd_fun.name}:");
+  String no = "345";
+  int pri = 30;
+  Map<String, int> new_number_in_angtd_fun = {no: pri};
 }
 
-bool duplicate_name(String name_for_testing) {
+int duplicate_name(String name_for_testing) {
   gambler_list_length = gambler_list.length;
   if (gambler_list_length > 0) {
     for (int i = 0; i < gambler_list_length; i++) {
       if (name_for_testing == gambler_list[i].name) {
-        return false;
+        return i;
       }
     }
-  } else {
-    return true;
   }
-  return true;
+  return -1; //-1 if doesn't duplicate
 }
 
 void change_name() {
-  String name_to_search = "name1";
+  String name_to_search = "name6";
   String new_name = "name_changed";
-  gambler_list_length = gambler_list.length;
-  for (int i = 0; i <= gambler_list_length; i++) {
-    if (gambler_list_length != i) {
-      if (name_to_search == gambler_list[i].name) {
-        gambler_list[i].name = new_name;
-        print("$name_to_search has changed to $new_name");
-        return;
-      }
-    } else {
-      print("name doesn't found");
-    }
+  pass = duplicate_name(name_to_search);
+  if (pass > -1) {
+    print("\n${gambler_list[pass].name} has changed into $new_name");
+    gambler_list[pass].name = new_name;
+  } else {
+    print("name don't found");
   }
 }
